31bfabd32db6e14a6da6faee453540dd0bf4c5d1
Guilherme Andrade <guilhermesantosandrade01@gmail.com>
2023-08-29 16:58:05 -0300

    08/29

---
+++ Curso_Java/src/exercicio_cap13_enumeracoes_composicao/Comment_post.java
@@ -0,0 +1,29 @@
+package exercicio_cap13_enumeracoes_composicao;
+
+public class Comment_post {
+
+	private String text;
+	
+	public Comment_post() {
+		
+		
+	}
+
+	public Comment_post(String text) {
+		
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+	
+	
+
+}
---
+++ Curso_Java/src/exercicio_cap13_enumeracoes_composicao/Program_post.java
@@ -0,0 +1,40 @@
+package exercicio_cap13_enumeracoes_composicao;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Program_post {
+
+	public static void main(String[] args) throws ParseException {
+
+		SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+		Comment_post c1= new Comment_post("Have a nice trip!");
+		Comment_post c2= new Comment_post("Wow that's awesome!");
+		
+		Post p1= new Post(sdf.parse("21/06/2018 13:05:44"),
+							"Traveling to New Zeeland",
+							"I'm going to visit this wonderful country!",
+							12);
+		
+		p1.addComment(c1);
+		p1.addComment(c2);
+
+		System.out.println(p1+ "\n.......................................\n");
+		
+		// post 2
+		Comment_post c3= new Comment_post("Good night!");
+		Comment_post c4= new Comment_post("May the Force be with you");
+		
+		Post p2= new Post(sdf.parse("28/07/2018 23:14:19"),
+							"Good night guys",
+							"See you tomorrow",
+							5);
+		
+		p2.addComment(c3);
+		p2.addComment(c4);
+
+		System.out.println(p2);
+	}
+
+}
